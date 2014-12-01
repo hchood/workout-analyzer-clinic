3c80a094a257d47b60fe0688d096b293f50ec334
Helen Hood <helen.c.hood@gmail.com>
2014-12-01 13:38:07 -0500

    Initial commit - CSV and runner file

---
+++ workout_summary.rb
@@ -0,0 +1,26 @@
+require_relative 'workout'
+require 'csv'
+
+# create an array of Workout objects
+def load_workout_data(filename)
+  workouts = []
+
+  CSV.foreach(filename, headers: true, header_converters: :symbol, converters: :numeric) do |row|
+    workout = workouts.find { |wo| wo.id == row[:workout_id] }
+
+    if workout.nil?
+      workout = Workout.new(date: row[:date])
+      workouts << workout
+    end
+
+    exercise = {
+      name: row[:exercise],
+      category: row[:category],
+      duration_in_min: row[:duration_in_min]
+    }
+
+    workout.exercises << exercise
+  end
+
+  workouts
+end
